1f29f079d3e30501c7a115025b52b47a5ccf0eb2
ZachTheSloth <zachwilliams.services@gmail.com>
2018-08-03 01:18:59 -0400

    Created cell template for entries, as well as an example cell.

---
+++ Weekly/ItemCell.swift
@@ -0,0 +1,30 @@
+//
+//  ItemCell.swift
+//  Weekly
+//
+//  Created by Zachary Williams on 8/3/18.
+//  Copyright © 2018 Zachary Williams. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell
+{
+
+    
+    
+    // Outlets (Labels)
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var expirationLabel: UILabel!
+    
+    // Outlets (Credit Slots)
+    @IBOutlet weak var creditSlot1ImageView: UIImageView!
+    @IBOutlet weak var creditSlot2ImageView: UIImageView!
+    @IBOutlet weak var creditSlot3ImageView: UIImageView!
+    @IBOutlet weak var creditSlot4ImageView: UIImageView!
+    @IBOutlet weak var creditSlot5ImageView: UIImageView!
+    @IBOutlet weak var creditSlot6ImageView: UIImageView!
+    
+    
+    
+}
--- Weekly/ViewController.swift
+++ Weekly/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct cellData
+{
+    let title : String?
+    let expiration : Int? // Measured in days until reset.
+    let maxCredits : Int?
+    let currentCredits : Int?
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class TableViewController: UITableViewController
+{
+    
+    
+    
+    // Array of all cell data.
+    var data : [cellData] = []
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
+        // Initialize data array with test data.
+        data = [cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 5)]
     }
-
-
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return data.count
+    }
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        // Grabbing the first view in the xib file.
+        let cell = Bundle.main.loadNibNamed("ItemCell", owner: self, options: nil)?.first as! ItemCell
+        
+        // Set up the cell data.
+        cell.titleLabel.text = data[indexPath.row].title
+        cell.expirationLabel.text = String(data[indexPath.row].expiration!)
+        // TODO: Add stuff for the credit count.
+        
+        return cell
+    }
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 120;
+    }
+    
+    
+    
 }
 
