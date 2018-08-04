8d33fe4697fc5d9701ee17ea67b82daf046955c9
ZachTheSloth <zachwilliams.services@gmail.com>
2018-08-04 18:03:29 -0400

    Added navigation bar & (nonfunctional) search bar.

--- Weekly/ViewController.swift
+++ Weekly/ViewController.swift
@@ -24,6 +24,8 @@ class TableViewController: UITableViewController
     // Array of all cell data.
     var data : [cellData] = []
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
     
     
     
@@ -35,33 +37,40 @@ class TableViewController: UITableViewController
                 cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 3),
                 cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 2),
                 cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 1)]
+        
+        configureSerachBar()
     }
     
     
     
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    func configureSerachBar()
     {
-        return data.count
+        // Instantiate the search controller.
+        navigationItem.searchController = searchController
+        
+        // Configuration
+        navigationItem.hidesSearchBarWhenScrolling = false // Hide/show search bar when scrolling.
+        searchController.searchBar.tintColor = .white  // Make the 'cancel' text white.
     }
     
     
     
     
+   // Constructs the requested cell by the table view, by applying the correct data to the xib template.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         // Grabbing the first view in the xib file.
         let cell = Bundle.main.loadNibNamed("ItemCell", owner: self, options: nil)?.first as! ItemCell
         let cellData = data[indexPath.row]
         
-        // Set up the cell data.
-        
+        // Set up the cell text data.
         cell.titleLabel.text = cellData.title
         cell.expirationLabel.text = String(cellData.expiration!)
         
+        // Set up the cell's credit slots with correct images.
         let maxCredits = cellData.maxCredits
         let currentCredits = cellData.currentCredits
-        
         var listOfCreditSlots = [cell.creditSlot1ImageView, cell.creditSlot2ImageView, cell.creditSlot3ImageView, cell.creditSlot4ImageView, cell.creditSlot5ImageView, cell.creditSlot6ImageView]
         
         for index in 0...(currentCredits! - 1)
@@ -77,10 +86,19 @@ class TableViewController: UITableViewController
     
     
     
+    // CONFIGURATION METHODS
+    
+    // Sets the height of the table cells.
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
-    {
-        return 120;
-    }
+    { return 120 }
+    
+    // Sets the number of rows in the table view (should be equal to the # of entries in the data).
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    { return data.count }
+    
+    // Style of status bar (light/dark).
+    override var preferredStatusBarStyle: UIStatusBarStyle
+    { return .lightContent }
     
     
     
