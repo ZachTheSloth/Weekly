6830243cc8b70f88607776779e2e917728ee4f7f
ZachTheSloth <zachwilliams.services@gmail.com>
2018-08-06 09:33:39 -0400

    Implemented MVC strucutre, with CoreData as the persistant model, and a Controller class to handle data interactions between the view and the model.

---
+++ Weekly/Controller.swift
@@ -0,0 +1,164 @@
+//
+//  Controller.swift
+//  Weekly
+//
+//  Created by Zachary Williams on 8/5/18.
+//  Copyright © 2018 Zachary Williams. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class Controller
+{
+    
+
+    
+    
+    static let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    static var context = appDelegate.persistentContainer.viewContext
+    
+    
+    
+    
+    init()
+    {}
+    
+    
+    
+    
+    // METHODS FOR RETRIEVING CELL DATA
+    
+    
+    
+    
+    static private func getCellData(cellIndex: Int) -> NSManagedObject?
+    {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "CellDataContainer")
+        do
+        {
+            let result = try context.fetch(request) as! [NSManagedObject]
+            return result[cellIndex]
+        }
+        catch
+        {
+            print("getCellData() fetch request failed.")
+            return nil
+        }
+    }
+    
+    
+    
+    
+    static func getCellDataContainerEntityCount() -> Int?
+    {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "CellDataContainer")
+        do
+        {
+            let result = try context.fetch(request) as! [NSManagedObject]
+            return result.count
+        }
+        catch
+        {
+            print("getCellData() fetch request failed.")
+            return nil
+        }
+    }
+    
+    
+    
+    
+    static func getCellTitle(cellIndex: Int) -> String
+    {
+        let cellDataContainer = getCellData(cellIndex: cellIndex)
+        return cellDataContainer?.value(forKey: "title") as! String
+    }
+    
+    
+    
+    
+    static func getCellResetCycleLength(cellIndex: Int) -> Int
+    {
+        let cellDataContainer = getCellData(cellIndex: cellIndex)
+        return cellDataContainer?.value(forKey: "resetCycleLength") as! Int
+    }
+    
+    
+    
+    
+    static func getCellDaysUntilReset(cellIndex: Int) -> Int
+    {
+        let cellDataContainer = getCellData(cellIndex: cellIndex)
+        return cellDataContainer?.value(forKey: "daysUntilReset") as! Int
+    }
+    
+    
+    
+    
+    static func getCellMaxCredits(cellIndex: Int) -> Int
+    {
+        let cellDataContainer = getCellData(cellIndex: cellIndex)
+        return cellDataContainer?.value(forKey: "maxCredits") as! Int
+    }
+    
+    
+    
+    
+    static func getCellCurrentCredits(cellIndex: Int) -> Int
+    {
+        let cellDataContainer = getCellData(cellIndex: cellIndex)
+        return cellDataContainer?.value(forKey: "currentCredits") as! Int
+    }
+    
+    
+    
+    
+    static func createCell(title: String, resetCycleLength: Int, maxCredits: Int)
+    {
+        // Create the new entity and CellDataContainer.
+        let entity = NSEntityDescription.entity(forEntityName: "CellDataContainer", in: context)
+        let newCellDataContainer = NSManagedObject(entity: entity!, insertInto: context)
+        
+        // Set up their attributes.
+        newCellDataContainer.setValue(title, forKey: "title")
+        newCellDataContainer.setValue(resetCycleLength, forKey: "resetCycleLength")
+        newCellDataContainer.setValue(resetCycleLength, forKey: "daysUntilReset")
+        newCellDataContainer.setValue(maxCredits, forKey: "maxCredits")
+        newCellDataContainer.setValue(maxCredits, forKey: "currentCredits")
+        
+        // Save the data.
+        saveData()
+    }
+    
+    
+    
+    
+    static func updateCell()
+    {
+        
+    }
+    
+    
+    
+    
+    static func removeCell(cellIndex: Int)
+    {
+        context.delete(getCellData(cellIndex: cellIndex)!)
+        saveData()
+    }
+    
+    
+    
+    
+    static func saveData()
+    {
+        do
+        { try context.save() }
+        catch
+        { print("Failed to save data context.") }
+    }
+    
+    
+    
+}
--- Weekly/ViewController.swift
+++ Weekly/ViewController.swift
@@ -8,50 +8,23 @@
 
 import UIKit
 
-struct cellData
-{
-    let title : String?
-    let expiration : Int? // Measured in days until reset.
-    let maxCredits : Int?
-    let currentCredits : Int?
-}
-
 class TableViewController: UITableViewController
 {
+
     
     
     
-    // Array of all cell data.
-    var data : [cellData] = []
-    
+    // Search controller.
     let searchController = UISearchController(searchResultsController: nil)
     
+    // Custom Colors
+    let jazzberryJam = UIColor(red:0.80, green:0.17, blue:0.39, alpha:1.00)
     
     
     
     override func viewDidLoad()
     {
-        // Initialize data array with test data.
-        data = [cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 5),
-                cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 4),
-                cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 3),
-                cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 2),
-                cellData(title: "item", expiration: 7, maxCredits: 6, currentCredits: 1)]
-        
         configureSerachBar()
-    }
-    
-    
-    
-    
-    func configureSerachBar()
-    {
-        // Instantiate the search controller.
-        navigationItem.searchController = searchController
-        
-        // Configure visuals.
-        navigationItem.hidesSearchBarWhenScrolling = false // Hide/show search bar when scrolling.
-        searchController.searchBar.tintColor = .white  // Make the 'cancel' text white.
     }
     
     
@@ -62,22 +35,31 @@ class TableViewController: UITableViewController
     {
         // Grabbing the first view in the xib file.
         let cell = Bundle.main.loadNibNamed("ItemCell", owner: self, options: nil)?.first as! ItemCell
-        let cellData = data[indexPath.row]
         
-        // Set up the cell text data.
-        cell.titleLabel.text = cellData.title
-        cell.expirationLabel.text = String(cellData.expiration!)
+        // Set up the title & expiration.
+        cell.titleLabel.text = Controller.getCellTitle(cellIndex: indexPath.row)
+        cell.expirationLabel.text = String(Controller.getCellDaysUntilReset(cellIndex: indexPath.row))
         
         // Set up the cell's credit slots with correct images.
-        let maxCredits = cellData.maxCredits
-        let currentCredits = cellData.currentCredits
-        var listOfCreditSlots = [cell.creditSlot1ImageView, cell.creditSlot2ImageView, cell.creditSlot3ImageView, cell.creditSlot4ImageView, cell.creditSlot5ImageView, cell.creditSlot6ImageView]
+        let maxCredits = Controller.getCellMaxCredits(cellIndex: indexPath.row)
+        let currentCredits = Controller.getCellCurrentCredits(cellIndex: indexPath.row)
+        var listOfCreditSlots = [cell.creditSlot1ImageView,
+                                 cell.creditSlot2ImageView,
+                                 cell.creditSlot3ImageView,
+                                 cell.creditSlot4ImageView,
+                                 cell.creditSlot5ImageView,
+                                 cell.creditSlot6ImageView]
         
-        for index in 0...(currentCredits! - 1)
+        for index in 0...(maxCredits - 1)
+        { listOfCreditSlots[index]?.image = UIImage(named: "credit_empty") }
+        
+        for index in 0...(currentCredits - 1)
         { listOfCreditSlots[index]?.image = UIImage(named: "credit_full") }
         
-        for index in currentCredits!...(maxCredits! - 1)
-        { listOfCreditSlots[index]?.image = UIImage(named: "credit_empty") }
+        // Set the selection color.
+        let bgColorView = UIView()
+        bgColorView.backgroundColor = jazzberryJam
+        cell.selectedBackgroundView = bgColorView
         
         // Return the constructed cell.
         return cell
@@ -88,17 +70,32 @@ class TableViewController: UITableViewController
     
     // CONFIGURATION METHODS
     
+    
     // Sets the height of the table cells.
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     { return 120 }
     
+    
     // Sets the number of rows in the table view (should be equal to the # of entries in the data).
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    { return data.count }
+    { return Controller.getCellDataContainerEntityCount()! }
+    
     
     // Style of status bar (light/dark).
     override var preferredStatusBarStyle: UIStatusBarStyle
     { return .lightContent }
+    
+    
+    // Style of the search bar.
+    func configureSerachBar()
+    {
+        // Instantiate the search controller.
+        navigationItem.searchController = searchController
+        
+        // Configure visuals.
+        navigationItem.hidesSearchBarWhenScrolling = true // Hide/show search bar when scrolling.
+        searchController.searchBar.tintColor = .white  // Make the 'cancel' text white.
+    }
     
     
     
